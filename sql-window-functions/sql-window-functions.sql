07693b383d35ea7d76be51413c089e8e23ac11ad
mohamedisakr <mohamedisakr@gmail.com>
2022-09-13 13:46:52 +0200

    row number & rank quiz

--- sql-window-functions/sql-window-functions.sql
+++ sql-window-functions/sql-window-functions.sql
@@ -46,9 +46,23 @@ FROM orders
 -- 		RANK() OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month', occurred_at)) AS row_num
 -- FROM orders
 
-SELECT id, account_id, occurred_at, 
-		DATE_TRUNC('month', occurred_at) AS month,
-		DENSE_RANK() OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month', occurred_at)) AS row_num
+-- SELECT id, account_id, occurred_at, 
+-- 		DATE_TRUNC('month', occurred_at) AS month,
+-- 		DENSE_RANK() OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month', occurred_at)) AS row_num
+-- FROM orders
+
+------------------------------------------------------
+-- 8. Quiz: ROW_NUMBER & RANK
+------------------------------------------------------
+
+-- Ranking Total Paper Ordered by Account
+-- Select the id, account_id, and total variable from the orders table, then create a column called 
+-- total_rank that ranks this total amount of paper ordered (from highest to lowest) for each account 
+-- using a partition. 
+-- Your final table should have these four columns.
+
+SELECT id, account_id, total,
+		RANK() OVER (PARTITION BY account_id ORDER BY total DESC) AS total_rank
 FROM orders
 
 
